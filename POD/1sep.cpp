ba28fe398e11355396e38cca36b90aa50907c8c8
avanishgupta07 <avanishguptaaza@gmail.com>
2024-09-01 23:03:02 +0530

    Add

--- POD/1sep.cpp
+++ POD/1sep.cpp
@@ -42,3 +42,18 @@
         result += max(sum1, sum2);
 
         return result;
+
+
+
+//         Given two sorted arrays of distinct integers arr1 and arr2. Each array may have some elements in common with the other array. Find the maximum sum of a path from the beginning of any array to the end of any array. You can switch from one array to another array only at the common elements.
+
+// Note:  When we switch from one array to other,  we need to consider the common element only once in the result.
+
+// Examples : 
+
+// Input: arr1 = [2, 3, 7, 10, 12] , arr2 = [1, 5, 7, 8]
+// Output: 35
+// Explanation: The path will be 1+5+7+10+12 = 35, where 1 and 5 come from arr2 and then 7 is common so we switch to arr1 and add 10 and 12.
+// Input: arr1 = [1, 2, 3] , arr2[] = [3, 4, 5]
+// Output: 15
+// Explanation: The path will be 1+2+3+4+5=15.
